830eb37e378dea91b02ff43dd08cdf97fd6febe1
Joyful Lee <git@joyfullee.me>
2025-05-07 17:36:45 -0400

    gitlab: Always use amd64 images
    That's all GitLab provides and it needs to be overridden for Apple
    Silicon. Map qemu-x86_64 into container for emulation. Add privileged
    mode for setting limits under emulated environment. Acceptable because
    it's not production.

--- manifests/service/gitlab.pp
+++ manifests/service/gitlab.pp
@@ -77,15 +77,18 @@ class nest::service::gitlab (
     }
     ->
     nest::lib::container { 'gitlab':
-      image   => $image,
-      cap_add => ['SYS_CHROOT'],
-      env     => ["GITLAB_OMNIBUS_CONFIG=from_file('/omnibus_config.rb')"],
-      publish => $publish,
-      volumes => [
+      image      => $image,
+      arch       => 'amd64', # all there is
+      cap_add    => ['SYS_CHROOT'],
+      env        => ["GITLAB_OMNIBUS_CONFIG=from_file('/omnibus_config.rb')"],
+      publish    => $publish,
+      privileged => $facts['profile']['architecture'] != 'amd64',
+      volumes    => [
         '/srv/gitlab/gitlab.rb:/omnibus_config.rb:ro',
         '/srv/gitlab/config:/etc/gitlab',
         '/srv/gitlab/logs:/var/log/gitlab',
         '/srv/gitlab/data:/var/opt/gitlab',
+        '/usr/bin/qemu-x86_64:/usr/bin/qemu-x86_64:ro',
       ],
     }
 
