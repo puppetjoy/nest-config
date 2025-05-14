cb68f830309c0ba882fe88bd106a7ce6e64319b9
Joyful Lee <git@joyfullee.me>
2025-05-14 16:30:19 -0400

    vagrant: Run plugin install as user

--- manifests/tool/vagrant.pp
+++ manifests/tool/vagrant.pp
@@ -6,5 +6,6 @@ class nest::tool::vagrant {
   exec { 'vagrant-plugin-install-libvirt':
     command => '/usr/bin/vagrant plugin install vagrant-libvirt',
     unless  => '/usr/bin/vagrant plugin list | /usr/bin/grep -q vagrant-libvirt',
+    user    => 'james',
   }
 }
