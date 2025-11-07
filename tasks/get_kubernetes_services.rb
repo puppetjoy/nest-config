c0a2be5041d802ae3fb30932de6e7fa7df578b5f
Joyful Lee <git@joyfullee.me>
2025-11-07 01:18:13 -0500

    k8s: Fix jump service with OpenSSH 10
    New major version is more strict with PAM config, which doesn't work in
    a container like this. Fix inventory plugin when jump service is
    unavailable.

--- tasks/get_kubernetes_services.rb
+++ tasks/get_kubernetes_services.rb
@@ -11,26 +11,27 @@ class GetKubernetesServices < TaskHelper
     return { value: [] } unless $CHILD_STATUS.success?
     services = JSON.parse(services)
 
-    {
-      value: services['items'].map do |service|
-        if service['spec']['type'] == 'LoadBalancer'
-          uri = service['metadata']['labels']['james.tl/fqdn']
-          config = {}
-        else
-          namespace = service['metadata']['namespace']
-          jump_namespace = (namespace == 'test') ? 'test' : 'default'
-          jump_service = services['items'].find { |s| s['metadata']['name'] == 'jump' && s['metadata']['namespace'] == jump_namespace }
-          uri = "#{service['metadata']['name']}.#{namespace}.svc.cluster.local"
-          config = { ssh: { proxyjump: jump_service['metadata']['labels']['james.tl/fqdn'] } }
-        end
-
-        {
-          name: service['metadata']['labels']['james.tl/service_name'],
-          uri: uri,
-          config: config,
-        }
+    targets = services['items'].map { |service|
+      if service['spec']['type'] == 'LoadBalancer'
+        uri = service['metadata']['labels']['james.tl/fqdn']
+        config = {}
+      else
+        namespace = service['metadata']['namespace']
+        jump_namespace = (namespace == 'test') ? 'test' : 'default'
+        jump_service = services['items'].find { |s| s['metadata']['name'] == 'jump' && s['metadata']['namespace'] == jump_namespace }
+        next nil unless jump_service
+        uri = "#{service['metadata']['name']}.#{namespace}.svc.cluster.local"
+        config = { ssh: { proxyjump: jump_service['metadata']['labels']['james.tl/fqdn'] } }
       end
-    }
+
+      {
+        name: service['metadata']['labels']['james.tl/service_name'],
+        uri: uri,
+        config: config,
+      }
+    }.compact
+
+    { value: targets }
   end
 end
 
