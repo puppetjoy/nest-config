65a6d3d1dbb33300dbfd10d7f82eb641d054f306
Joyful Lee <git@joyfullee.me>
2025-11-10 13:49:52 -0500

    network: Restore wait online for any only for wifi
    Even with the resolved reset workaround, the resolver starts in a bad
    state on kestrel due to wait-online returning too soon, probably because
    it has some non-routable interfaces. This wait-for-any hack was only
    ever for systems with wifi, so let's apply the original intent.

--- manifests/base/network.pp
+++ manifests/base/network.pp
@@ -45,27 +45,36 @@ class nest::base::network {
     }
   }
 
-  $wait_for_any_online = @(WAIT)
-    [Service]
-    ExecStart=
-    ExecStart=/lib/systemd/systemd-networkd-wait-online --any
-    | WAIT
+  if $nest::wifi {
+    $wait_for_any_online = @(WAIT)
+      [Service]
+      ExecStart=
+      ExecStart=/lib/systemd/systemd-networkd-wait-online --any
+      | WAIT
 
-  file {
-    default:
-      mode  => '0644',
-      owner => 'root',
-      group => 'root',
-    ;
+    file {
+      default:
+        mode  => '0644',
+        owner => 'root',
+        group => 'root',
+      ;
 
-    '/etc/systemd/system/systemd-networkd-wait-online.service.d':
-      ensure => directory,
-    ;
+      '/etc/systemd/system/systemd-networkd-wait-online.service.d':
+        ensure => directory,
+      ;
 
-    '/etc/systemd/system/systemd-networkd-wait-online.service.d/10-wait-for-any.conf':
-      content => $wait_for_any_online,
-    ;
+      '/etc/systemd/system/systemd-networkd-wait-online.service.d/10-wait-for-any.conf':
+        content => $wait_for_any_online,
+      ;
+    }
+    ~>
+    nest::lib::systemd_reload { 'network': }
+  } else {
+    file { '/etc/systemd/system/systemd-networkd-wait-online.service.d':
+      ensure => absent,
+      force  => true,
+    }
+    ~>
+    nest::lib::systemd_reload { 'network': }
   }
-  ~>
-  nest::lib::systemd_reload { 'network': }
 }
