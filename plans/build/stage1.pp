85c32a888218eb072ebdde7bc5b6c2a63a4fb096
Joyful Lee <git@joyfullee.me>
2025-12-06 02:42:04 -0500

    stage1: Update workstation circular dependency resolution

--- plans/build/stage1.pp
+++ plans/build/stage1.pp
@@ -122,16 +122,11 @@ plan nest::build::stage1 (
     # Resolve circular dependencies
     if $variant == 'workstation' and !$refresh {
       run_command('emerge --oneshot --verbose media-libs/harfbuzz media-libs/freetype media-libs/mesa', $target, 'Resolve media circular dependencies', _env_vars => {
-        'USE' => '-cairo -harfbuzz -truetype -vaapi',
+        'USE' => '-cairo -harfbuzz -truetype -vaapi -webp',
       })
       run_command('emerge --oneshot --verbose x11-misc/xdg-utils', $target, 'Resolve Plasma circular dependencies', _env_vars => {
         'USE' => '-plasma',
       })
-    }
-
-    # XXX Cleanup
-    if $refresh {
-      run_command('emerge --unmerge sys-fs/zfs', $target, 'Clean up ZFS before refresh', _catch_errors => true)
     }
 
     # Make the system consistent with the profile
