f7196147df558e407716a6d99f13df3d27880ea7
Joyful Lee <git@joyfullee.me>
2025-12-15 17:20:19 -0500

    kestrel-console: Apply tty settings to all USB-serial devices
    This issue affects both types of adapters attached to the system.

--- manifests/host/kestrel_console.pp
+++ manifests/host/kestrel_console.pp
@@ -1,13 +1,10 @@
 class nest::host::kestrel_console {
   $udev_rule_content = @(UDEV)
-    # CP2102 (10c4:ea60, serial 0001): force known-good tty settings on add
     ACTION=="add", SUBSYSTEM=="tty", KERNEL=="ttyUSB*", \
-      ENV{ID_BUS}=="usb", ENV{ID_VENDOR_ID}=="10c4", ENV{ID_MODEL_ID}=="ea60", \
-      ENV{ID_SERIAL_SHORT}=="0001", \
       RUN+="/bin/stty -F /dev/%k 115200 raw -echo -ixon -ixoff -crtscts"
     | UDEV
 
-  file { '/etc/udev/rules.d/99-cp2102-stty.rules':
+  file { '/etc/udev/rules.d/99-ttyusb-raw.rules':
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
