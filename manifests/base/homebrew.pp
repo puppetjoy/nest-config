1b87eb6317571271459641fae0d67969c00e9956
Joyful Lee <git@joyfullee.me>
2026-03-12 00:45:13 -0400

    homebrew: Fix install resources

--- manifests/base/homebrew.pp
+++ manifests/base/homebrew.pp
@@ -11,4 +11,25 @@ class nest::base::homebrew {
     provider == 'homebrew' or
     provider == undef
   |>
+
+
+  #
+  # Module fixes
+  #
+  include homebrew::install
+
+  # Install can't run through 'su'
+  Exec <| title == 'install-homebrew' |> {
+    command     => $homebrew::install::homebrew_install_cmd,
+    environment => "HOME=/Users/${homebrew::user}",
+    user        => $homebrew::user,
+  }
+
+  Exec <| tag == 'homebrew::install' and title != 'install-homebrew' |> {
+    require => Exec['install-homebrew'],
+  }
+
+  File <| tag == 'homebrew::install' |> {
+    require => Exec['install-homebrew'],
+  }
 }
