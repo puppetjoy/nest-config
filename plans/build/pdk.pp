0d79b5c1c3818ba53a0907723268574dec35c36c
Joyful Lee <git@joyfullee.me>
2026-03-19 22:55:13 -0400

    pdk: skip bundle during test module creation
    Avoid PDK's implicit ensure_bundle! path during the module-creation smoke
    step. The build plan already runs bundle install and pdk validate
    explicitly afterward.

--- plans/build/pdk.pp
+++ plans/build/pdk.pp
@@ -60,7 +60,7 @@ plan nest::build::pdk (
   if $build {
     run_command("podman start ${container}", 'localhost', 'Start build container')
 
-    run_command('pdk new module --skip-interview build /module', $target, 'Create test module', _env_vars => $puppetcore_env_vars)
+    run_command('pdk new module --skip-interview --skip-bundle-install build /module', $target, 'Create test module', _env_vars => $puppetcore_env_vars)
     run_command('sh -c "cd /module && bundle install && pdk validate"', $target, 'Install gems and validate module', _env_vars => $puppetcore_env_vars)
     run_command('rm -rf /module && mkdir /module', $target, 'Clean up test module')
 
