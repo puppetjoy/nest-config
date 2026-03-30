23ad8d60086bdca16b3622de22799901c72c42fc
Joyful Lee <git@joyfullee.me>
2026-03-30 13:45:56 -0400

    gnome: Install docutils for mutter configure failure

--- manifests/gui/gnome.pp
+++ manifests/gui/gnome.pp
@@ -1,4 +1,9 @@
 class nest::gui::gnome {
+  # Mutter has an unexpressed dependency on rst2man
+  nest::lib::package { 'dev-python/docutils':
+    ensure => installed,
+  }
+  ->
   nest::lib::package { 'gnome-base/gnome':
     ensure => installed,
   }
