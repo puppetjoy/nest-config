6d13d00f17861de21ba40097575422da53373cef
Joyful Lee <git@joyfullee.me>
2026-03-31 14:53:25 -0400

    gui: configure gnome keyboard layout defaults
    Manage GNOME input source defaults declaratively from nest::dvorak
    and nest::swap_alt_win.
    
    Configure both the normal user dconf database and the GDM greeter
    profile so the selected layout and XKB options apply consistently
    for GNOME sessions.

--- manifests/gui/gnome.pp
+++ manifests/gui/gnome.pp
@@ -7,8 +7,96 @@ class nest::gui::gnome {
   nest::lib::package { 'gnome-base/gnome':
     ensure => installed,
   }
-  ->
+
+  $keyboard_source = $nest::dvorak ? {
+    true    => 'us+dvorak',
+    default => 'us',
+  }
+
+  $xkb_options = $nest::swap_alt_win ? {
+    true    => "['ctrl:nocaps', 'altwin:swap_alt_win']",
+    default => "['ctrl:nocaps']",
+  }
+
+  $input_sources_content = @("END")
+    [org/gnome/desktop/input-sources]
+    sources=[('xkb', '${keyboard_source}')]
+    xkb-options=${xkb_options}
+    | END
+
+  $user_profile_content = @("END")
+    user-db:user
+    system-db:local
+    | END
+
+  $gdm_profile_content = @("END")
+    user-db:user
+    system-db:gdm
+    file-db:/usr/share/gdm/greeter-dconf-defaults
+    | END
+
+  exec { 'dconf-update':
+    command     => '/usr/bin/dconf update',
+    refreshonly => true,
+    require     => Nest::Lib::Package['gnome-base/gnome'],
+  }
+
+  file {
+    default:
+      owner   => 'root',
+      group   => 'root',
+      require => Nest::Lib::Package['gnome-base/gnome'],
+      notify  => Exec['dconf-update'],
+    ;
+
+    '/etc/dconf':
+      ensure => directory,
+      mode   => '0755',
+    ;
+
+    '/etc/dconf/db':
+      ensure => directory,
+      mode   => '0755',
+    ;
+
+    '/etc/dconf/db/gdm.d':
+      ensure => directory,
+      mode   => '0755',
+    ;
+
+    '/etc/dconf/db/gdm.d/00-input-sources':
+      mode    => '0644',
+      content => $input_sources_content,
+    ;
+
+    '/etc/dconf/db/local.d':
+      ensure => directory,
+      mode   => '0755',
+    ;
+
+    '/etc/dconf/db/local.d/00-input-sources':
+      mode    => '0644',
+      content => $input_sources_content,
+    ;
+
+    '/etc/dconf/profile':
+      ensure => directory,
+      mode   => '0755',
+    ;
+
+    '/etc/dconf/profile/gdm':
+      mode    => '0644',
+      content => $gdm_profile_content,
+    ;
+
+    '/etc/dconf/profile/user':
+      mode    => '0644',
+      content => $user_profile_content,
+    ;
+  }
+
   service { 'gdm':
-    enable => true,
+    enable  => true,
+    require => Nest::Lib::Package['gnome-base/gnome'],
   }
 }
