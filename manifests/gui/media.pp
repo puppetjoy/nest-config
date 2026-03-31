2924d128f90bcb118d00a471612d7282a4803aee
Joyful Lee <git@joyfullee.me>
2026-03-31 16:25:32 -0400

    gui: manage absent VA-API packages

--- manifests/gui/media.pp
+++ manifests/gui/media.pp
@@ -1,4 +1,7 @@
 class nest::gui::media {
+  $vaapi_enabled = 'vaapi' in $facts['portage_use'].split(' ')
+  $intel_enabled = 'intel' in $facts['portage_video_cards'].split(' ')
+
   nest::lib::package { [
     'media-sound/playerctl',
     'media-video/mpv',
@@ -6,15 +9,17 @@ class nest::gui::media {
     ensure => installed,
   }
 
-  if 'vaapi' in $facts['portage_use'].split(' ') {
-    nest::lib::package { 'media-video/libva-utils':
-      ensure => installed,
-    }
+  nest::lib::package { 'media-video/libva-utils':
+    ensure => $vaapi_enabled ? {
+      true    => installed,
+      default => absent,
+    },
+  }
 
-    if 'intel' in $facts['portage_video_cards'].split(' ') {
-      nest::lib::package { 'media-libs/libva-intel-driver':
-        ensure => installed,
-      }
-    }
+  nest::lib::package { 'media-libs/libva-intel-driver':
+    ensure => ($vaapi_enabled and $intel_enabled) ? {
+      true    => installed,
+      default => absent,
+    },
   }
 }
