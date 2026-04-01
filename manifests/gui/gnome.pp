6a5815193b9b2e7182fb5466ecc1341593fd62ac
Joyful Lee <git@joyfullee.me>
2026-03-31 23:54:40 -0400

    gui: manage GNOME account avatar
    Add AccountsService state for the primary desktop user so GDM and GNOME
    Shell can pick up the avatar from the dotfiles checkout.
    
    Keep the dependency at the nest::base::users class boundary and manage the
    AccountsService directories with the observed local permissions.

--- manifests/gui/gnome.pp
+++ manifests/gui/gnome.pp
@@ -37,6 +37,7 @@ class nest::gui::gnome {
     require     => Nest::Lib::Package['gnome-base/gnome'],
   }
 
+  # System dconf defaults
   file {
     default:
       owner   => 'root',
@@ -73,6 +74,49 @@ class nest::gui::gnome {
     '/etc/dconf/profile/user':
       mode    => '0644',
       content => $user_profile_content,
+    ;
+  }
+
+  $accountsservice_icon = "/var/lib/AccountsService/icons/${nest::user}"
+
+  $accountsservice_user_content = @("END")
+    [User]
+    Icon=${accountsservice_icon}
+    SystemAccount=false
+    | END
+
+  # Account avatar shown by GDM and GNOME Shell
+  file {
+    default:
+      owner   => 'root',
+      group   => 'root',
+    ;
+
+    '/var/lib/AccountsService':
+      ensure => directory,
+      mode   => '0775',
+    ;
+
+    '/var/lib/AccountsService/icons':
+      ensure => directory,
+      mode   => '0775',
+    ;
+
+    '/var/lib/AccountsService/users':
+      ensure => directory,
+      mode   => '0700',
+    ;
+
+    $accountsservice_icon:
+      mode    => '0644',
+      source  => "/home/${nest::user}/.face.icon",
+      require => Class['nest::base::users'],
+    ;
+
+    "/var/lib/AccountsService/users/${nest::user}":
+      mode    => '0600',
+      content => $accountsservice_user_content,
+      require => File[$accountsservice_icon],
     ;
   }
 
