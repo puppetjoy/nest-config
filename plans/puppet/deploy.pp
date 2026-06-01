01be00ad990b90c8a70bdfc6c91212c9aec5154c
Joyful Lee <git@joyfullee.me>
2026-06-01 13:35:30 -0400

    puppet: keep deploy plan transitional
    Keep nest::puppet::deploy in the migration middle ground: deploy to
    legacy puppet-server and the OpenVox test namespace by default, but do
    not touch production Kubernetes yet. Preserve the old all-environments
    behavior when no environment is specified, while allowing CI or a human
    to pass a specific branch/environment explicitly.

--- plans/puppet/deploy.pp
+++ plans/puppet/deploy.pp
@@ -1,25 +1,50 @@
-# Deploy the Puppet control repo into the Kubernetes OpenVox service
-plan nest::puppet::deploy () {
-  $namespace   = 'default'
-  $deployment  = 'deploy/puppet-puppetserver-puppetserver-master'
-  $environment = 'main'
+# Deploy the Puppet control repo during the OpenVox migration
+plan nest::puppet::deploy (
+  Enum['legacy', 'test', 'both'] $backend = 'both',
+  Optional[String[1]]             $environment = undef,
+) {
+  $environment_args = $environment ? {
+    undef   => [],
+    default => [$environment],
+  }
 
-  $deploy_cmd = [
-    'kubectl', 'exec', '-n', $namespace, $deployment,
-    '-c', 'r10k-code', '--',
-    '/container-entrypoint.sh', 'deploy', 'environment', $environment,
-    '--config', '/etc/puppetlabs/puppet/r10k_code.yaml',
-    '--puppetfile',
-  ].shellquote
+  if $backend in ['legacy', 'both'] {
+    $legacy_cmd = (['/srv/puppet/bin/r10k', 'deploy', 'environment'] + $environment_args + ['-pv']).shellquote
 
-  run_command($deploy_cmd, 'localhost', 'Deploy Puppet code')
+    run_command($legacy_cmd, 'puppet-server', 'Deploy Puppet code to legacy puppet-server', '_run_as' => 'root')
+  }
 
-  $verify_cmd = [
-    'kubectl', 'exec', '-n', $namespace, $deployment,
-    '-c', 'r10k-code', '--',
-    'git', '-C', "/etc/puppetlabs/code/environments/${environment}",
-    'rev-parse', '--short', 'HEAD',
-  ].shellquote
+  if $backend in ['test', 'both'] {
+    $namespace  = 'test'
+    $deployment = 'deploy/puppet-puppetserver-puppetserver-master'
 
-  run_command($verify_cmd, 'localhost', 'Verify Puppet code deployment')
+    $deploy_cmd = ([
+      'kubectl', 'exec', '-n', $namespace, $deployment,
+      '-c', 'r10k-code', '--',
+      '/container-entrypoint.sh', 'deploy', 'environment',
+    ] + $environment_args + [
+      '--config', '/etc/puppetlabs/puppet/r10k_code.yaml',
+      '--puppetfile',
+    ]).shellquote
+
+    run_command($deploy_cmd, 'localhost', 'Deploy Puppet code to test OpenVox')
+
+    if $environment =~ String[1] {
+      $verify_cmd = [
+        'kubectl', 'exec', '-n', $namespace, $deployment,
+        '-c', 'r10k-code', '--',
+        'git', '-C', "/etc/puppetlabs/code/environments/${environment}",
+        'rev-parse', '--short', 'HEAD',
+      ].shellquote
+    } else {
+      $verify_cmd = [
+        'kubectl', 'exec', '-n', $namespace, $deployment,
+        '-c', 'r10k-code', '--',
+        '/bin/sh', '-c',
+        'for env in /etc/puppetlabs/code/environments/*; do [ -d "$env" ] && basename "$env"; done | sort',
+      ].shellquote
+    }
+
+    run_command($verify_cmd, 'localhost', 'Verify test OpenVox Puppet code deployment')
+  }
 }
