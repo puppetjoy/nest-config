b0bfb2748aae81bb5190d52a820d45c0b32ac339
Talon <talon@joyfullee.me>
2026-06-11 04:05:11 -0400

    gui: replace screenshot helper stack with Flameshot
    Install Flameshot as the managed desktop screenshot tool and stop
    managing the custom grim/slurp/swappy helper stack. Keep Shutter and
    Spectacle absent so the desktop workflow has one obvious X11 screenshot
    entry point for the new XMonad binding.

--- manifests/gui/screenshots.pp
+++ manifests/gui/screenshots.pp
@@ -1,19 +1,15 @@
 class nest::gui::screenshots {
+  nest::lib::package { 'media-gfx/flameshot':
+    ensure => installed,
+  }
+
   nest::lib::package { [
     'gui-apps/grim',
     'gui-apps/slurp',
     'gui-apps/swappy',
-    'media-gfx/imagemagick',
-    'x11-misc/xclip',
+    'x11-misc/shutter',
+    'kde-plasma/spectacle',
   ]:
-    ensure => installed,
-  }
-
-  nest::lib::package { 'x11-misc/shutter':
-    ensure => absent,
-  }
-
-  nest::lib::package { 'kde-plasma/spectacle':
     ensure => absent,
   }
 }
