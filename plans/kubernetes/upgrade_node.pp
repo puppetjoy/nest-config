847dd0e4043568f4f04326809eacf78f2d27003b
Talon <talon@joyfullee.me>
2026-08-09 12:40:45 -0400

    kubernetes: run node upgrades as root

--- plans/kubernetes/upgrade_node.pp
+++ plans/kubernetes/upgrade_node.pp
@@ -4,11 +4,19 @@
 plan nest::kubernetes::upgrade_node (
   TargetSpec $targets
 ) {
-  run_command('eix-sync -a', $targets)
+  run_command('eix-sync -a', $targets, {
+    _run_as => 'root',
+  })
 
   get_targets($targets).each |$target| {
-    run_command('kubeadm upgrade node --ignore-preflight-errors=SystemVerification --patches=/etc/kubernetes/patches', $target)
-    run_command('emerge --oneshot --verbose kubelet', $target)
-    run_command('systemctl restart kubelet', $target)
+    run_command('kubeadm upgrade node --ignore-preflight-errors=SystemVerification --patches=/etc/kubernetes/patches', $target, {
+      _run_as => 'root',
+    })
+    run_command('emerge --oneshot --verbose kubelet', $target, {
+      _run_as => 'root',
+    })
+    run_command('systemctl restart kubelet', $target, {
+      _run_as => 'root',
+    })
   }
 }
