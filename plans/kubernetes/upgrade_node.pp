146a3abf9b86a57e4faa84cb133cf9a257e67f39
Talon <talon@joyfullee.me>
2026-08-09 12:44:41 -0400

    kubernetes: allow kubeadm-only node reconciliation

--- plans/kubernetes/upgrade_node.pp
+++ plans/kubernetes/upgrade_node.pp
@@ -2,21 +2,27 @@
 #
 # @param targets Nodes to upgrade
 plan nest::kubernetes::upgrade_node (
-  TargetSpec $targets
+  TargetSpec $targets,
+  Boolean    $sync            = true,
+  Boolean    $upgrade_kubelet = true,
 ) {
-  run_command('eix-sync -a', $targets, {
-    _run_as => 'root',
-  })
+  if $sync {
+    run_command('eix-sync -a', $targets, {
+      _run_as => 'root',
+    })
+  }
 
   get_targets($targets).each |$target| {
     run_command('kubeadm upgrade node --ignore-preflight-errors=SystemVerification --patches=/etc/kubernetes/patches', $target, {
       _run_as => 'root',
     })
-    run_command('emerge --oneshot --verbose kubelet', $target, {
-      _run_as => 'root',
-    })
-    run_command('systemctl restart kubelet', $target, {
-      _run_as => 'root',
-    })
+    if $upgrade_kubelet {
+      run_command('emerge --oneshot --verbose kubelet', $target, {
+        _run_as => 'root',
+      })
+      run_command('systemctl restart kubelet', $target, {
+        _run_as => 'root',
+      })
+    }
   }
 }
