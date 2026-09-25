880520fa2e9fddb012f537598e127d81288752f2
Talon <talon@joyfullee.me>
2026-09-25 13:35:31 -0400

    dns: route home queries through new gateway

--- manifests/lib/router.pp
+++ manifests/lib/router.pp
@@ -10,6 +10,7 @@ class nest::lib::router {
     dhcp-option=option:classless-static-route,0.0.0.0/0,172.22.4.1,172.22.0.0/24,172.22.4.3
     local=/nest/
     local=/eyrie/
+    server=/home/172.22.1.1
     server=172.22.4.1
     | EOT
 
